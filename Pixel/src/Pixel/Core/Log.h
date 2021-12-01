aa4740ed710dd1bcb24163cac5755c96387d688a
pixel-Teee <1173769234@qq.com>
2021-12-01 13:45:10 +0800

    add cmake

--- Pixel/src/Pixel/Core/Log.h
+++ Pixel/src/Pixel/Core/Log.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #include "Core.h"
-#include "spdlog/spdlog.h"
-#include "spdlog/fmt/ostr.h"
+#include <spdlog/spdlog.h>
+#include <spdlog/fmt/ostr.h>
 
 namespace Pixel {
 	class PIXEL_API Log
