d42274eb20fb19f3b8f7ee5b573992edd9bd4ea4
pixel-Teee <1173769234@qq.com>
2021-12-19 16:55:51 +0800

    update buffer.cpp

--- Pixel/src/Pixel/Renderer/Buffer.cpp
+++ Pixel/src/Pixel/Renderer/Buffer.cpp
@@ -24,7 +24,7 @@ namespace Pixel {
 		switch (Renderer::GetAPI())
 		{
 		case RendererAPI::API::None:	  PX_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
-		case RendererAPI::API::OpenGL:    return std::make_shared<OpenGLVertexBuffer>(new OpenGLVertexBuffer(size));
+		case RendererAPI::API::OpenGL:    return CreateRef<OpenGLVertexBuffer>(size);
 		}
 
 		PX_CORE_ASSERT(false, "Unknown RendererAPI!");
