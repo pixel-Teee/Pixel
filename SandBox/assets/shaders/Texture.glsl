64e2b369e46fff99f35110cf84fd88d99a12a1d8
pixel-Teee <1173769234@qq.com>
2021-12-19 18:29:25 +0800

    modify Texture.glsl

--- SandBox/assets/shaders/Texture.glsl
+++ SandBox/assets/shaders/Texture.glsl
@@ -4,24 +4,27 @@
 #version 330 core
 
 layout(location = 0) in vec3 a_Position;
-layout(location = 1) in vec2 a_TexCoord;
+layout(location = 1) in vec4 a_Color;
+layout(location = 2) in vec2 a_TexCoord;
 
 uniform mat4 u_ViewProjection;
-uniform mat4 u_Transform;
 
+out vec4 v_Color;
 out vec2 v_TexCoord;
 
 void main()
 {
 	v_TexCoord = a_TexCoord;
-	gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
+	v_Color = a_Color;
+	gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
 }
 
 #type fragment
 #version 330 core
 
-layout(location = 0) out vec4 color;
+out vec4 color;
 
+in vec4 v_Color;
 in vec2 v_TexCoord;
 
 uniform vec4 u_Color;
@@ -30,6 +33,7 @@ uniform sampler2D u_Texture;
 
 void main()
 {
-	color = texture(u_Texture, v_TexCoord * u_TilingFactor) * u_Color;
+	//color = texture(u_Texture, v_TexCoord * u_TilingFactor) * u_Color;
 	//color = vec4(v_TexCoord, 0.0f, 1.0f);
+	color = v_Color;
 }
