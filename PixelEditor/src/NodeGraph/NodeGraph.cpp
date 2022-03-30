12ceee3aafcab8603532714e08fa9a94725948cf
pixel-Teee <1173769234@qq.com>
2022-03-30 22:37:20 +0800

    first add temp node editor

--- PixelEditor/src/NodeGraph/NodeGraph.cpp
+++ PixelEditor/src/NodeGraph/NodeGraph.cpp
@@ -1,12 +1,36 @@
 #include "pxpch.h"
 #include "NodeGraph.h"
-#include "Pixel/Renderer/3D/MaterialPass.h"
 
 namespace Pixel {
 
 	NodeGraph::NodeGraph()
 	{
-		model = Model("assets/models/Box.obj");
+		m_previewBox = Model("assets/models/Box.obj");
+		
+		m_pStaticMesh = CreateRef<StaticMesh>(m_previewBox.GetMeshes()[0]);
+		
+		m_pMaterial = CreateRef<Material>("Test", Material::MUT_PBR);
+		m_pMaterialInstance = CreateRef<MaterialInstance>(m_pMaterial);
+
+		m_materialPass.SetCamera(m_pCamera);
+		m_materialPass.SetMaterialInstance(m_pMaterialInstance);
+		m_materialPass.SetMesh(m_pStaticMesh);
+
+		//------Node Graph------
+		m_isopen = false;
+		m_isFirstFrame = true;
+		m_uniqueId = 0;
+		
+		ed::Config config;
+		config.SettingsFile = "BasicInteraction.json";
+		g_Context = ed::CreateEditor(&config);
+		ed::SetCurrentEditor(g_Context);
+		//------Node Graph------
+	}
+
+	NodeGraph::~NodeGraph()
+	{
+		ed::DestroyEditor(g_Context);
 	}
 
 	void NodeGraph::OnImGuiRender()
@@ -14,19 +38,42 @@ namespace Pixel {
 		ImGui::Begin("Test Button");
 		if (ImGui::Button("Test"))
 		{
-			MaterialPass pass;//test
-			std::vector<StaticMesh> meshes = model.GetMeshes();
-			Ref<StaticMesh> mesh = CreateRef<StaticMesh>(meshes[0]);
-			//Ref<StaticMesh> mesh = CreateRef<StaticMesh>();
-			Ref<Camera> pCamera = CreateRef<Camera>();
-			Ref<Material> tempMaterial = CreateRef<Material>("Test", Material::MUT_PBR);
-			Ref<MaterialInstance> pMaterialInstance = CreateRef<MaterialInstance>(tempMaterial);
-			pass.SetCamera(pCamera);
-			pass.SetMaterialInstance(pMaterialInstance);
-			pass.SetMesh(mesh);
-			pass.Draw();
+			if (m_isopen) m_isopen = false;
+			else m_isopen = true;
 		}
 		ImGui::End();
+
+		if (m_isopen)
+		{
+			ImGui::Begin("Context");
+			ed::Begin("My Editor", ImVec2(0.0, 0.0f));
+
+			m_uniqueId = 1;
+			//Submit Node A
+			ed::NodeId nodeA_Id = ++m_uniqueId;
+			ed::PinId nodeA_InputPinId = ++m_uniqueId;
+			ed::PinId nodeA_OutputPinId = ++m_uniqueId;
+
+			if (m_isFirstFrame)
+			{
+				m_isFirstFrame = false;
+
+				ed::SetNodePosition(nodeA_Id, ImVec2(10, 10));
+			}
+
+			ed::BeginNode(nodeA_Id);
+				ImGui::Text("Node A");
+				ed::BeginPin(nodeA_InputPinId, ed::PinKind::Input);
+					ImGui::Text("-> In");
+				ed::EndPin();
+				ImGui::SameLine();
+				ed::BeginPin(nodeA_OutputPinId, ed::PinKind::Output);
+					ImGui::Text("Out ->");
+					ed::EndPin();
+			ed::EndNode();
+			ed::End();
+			ImGui::End();
+		}
 	}
 
 }
--- PixelEditor/src/NodeGraph/NodeGraph.h
+++ PixelEditor/src/NodeGraph/NodeGraph.h
@@ -1,15 +1,36 @@
 #pragma once
 
 #include "Pixel/Renderer/3D/Model.h"
+#include "Pixel/Renderer/3D/MaterialPass.h"
 #include "imgui-node-editor/imgui_node_editor.h"
 
 namespace Pixel {
+	namespace ed = ax::NodeEditor;
+
 	class NodeGraph {
 	public:
 		NodeGraph();
-
+		~NodeGraph();
 		void OnImGuiRender();
 	private:
-		Model model;
+		Model m_previewBox;
+
+		//TODO:move to scene
+		MaterialPass m_materialPass;
+
+		Model m_model;
+
+		Ref<Camera> m_pCamera;
+
+		Ref<StaticMesh> m_pStaticMesh;
+
+		Ref<Material> m_pMaterial;
+		Ref<MaterialInstance> m_pMaterialInstance;
+
+		bool m_isopen;
+		bool m_isFirstFrame;
+
+		int m_uniqueId;
+		ed::EditorContext* g_Context = nullptr;
 	};
 }
